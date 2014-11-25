0116f8b17a7910ca4fe8a917f067de70f503bfa5
Thibs <bn.thibault@gmail.com>
2014-11-25 16:10:28 +0100

    Verification of URL and ICS in api.py and log, add cache in /tmp/ics-api-cache closed #5 closed #2 closed #4

--- api/api.py
+++ api/api.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify, request
+from helpers.log import get_status_code
+from werkzeug.exceptions import BadRequest
+from icalmanage.icalparser import ical_to_dict
+from requests.exceptions import MissingSchema, InvalidURL
+import requests
+import requests_cache
 
 app = Flask(__name__)
+
+requests_cache.install_cache('/tmp/ics-api-cache', expire_after=600)
 
 
 @app.route('/')
@@ -8,23 +16,23 @@ def index():
     return "Hello world"
 
 
-dic = [
-    {
-        'event': 'toto',
-        'date': '29/01/14',
-        'hour': '16h',
-        'part': [
-            'toto',
-            'titi',
-            'tata'
-        ]
-    }
-]
-
 @app.route('/api/get/', methods=['GET'])
 def get():
-    #return jsonify({'event': dic})
-    return request.args.get('url')
+    get_return_request = request.args.get('url', "")
+    try:
+        r = requests.get(get_return_request, stream=True)
+    except MissingSchema:
+        raise BadRequest("Bad URL Provided")
+    except InvalidURL:
+        raise BadRequest("Bad URL Provided")
+    if get_status_code(r):
+        event = ical_to_dict(r)
+        if not event:
+            raise BadRequest("Bad ICS Provided")
+        return jsonify(event)
+    else:
+        raise BadRequest("HTTP Exception")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
