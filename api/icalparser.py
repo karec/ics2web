0db9c8e96202b8550f7d538314617328ba5a8e14
karec <manu.valette@gmail.com>
2014-11-25 11:26:52 +0100

    add events object to ret list ref #3

--- api/icalparser.py
+++ api/icalparser.py
@@ -30,14 +30,14 @@ def ical_to_dict(stream):
                 event = {'place': ev.get('LOCATION').to_ical(),
                          'name': ev.get('SUMMARY').to_ical(),
                          'personnes': to_log(ev.get('ATTENDEE'))}
-                print event
-                print ev.get('LOCATION')
+                ret.append(event)
     next_ev = [{'name': ev.get('SUMMARY').to_ical(), 'place': ev.get('LOCATION').to_ical().replace('\\', ''),
                 'end': str(ev.get('DTEND').dt),
                 'start': str(ev.get('DTSTART').dt)}
                for ev in cal.walk()
                if ev.name == "VEVENT" and (now < ev.get('DTSTART').dt <= day_end)]
     print next_ev
+    print ret
 
 
 r = requests.get('https://www.google.com/calendar/ical/valett_e%40etna-alternance.net/private-dcbad4791bccb7846db0fdd38f9498f8/basic.ics', stream=True)
