801d7209e5ea61e0bd138ff336fe7a73d267b971
karec <manu.valette@gmail.com>
2014-11-27 12:20:51 +0100

    adding deployement facilities

--- deployement/fabfile.py
+++ deployement/fabfile.py
@@ -1,22 +1,25 @@
-from fabric.api import sudo, cd, run
+from fabric.api import sudo, cd, run, env
 from fabric.colors import green, yellow
+from fabric.contrib.files import append
 import ConfigParser
 
+env.hosts = ['10.104.10.63']
 
-def install_api_serveur(conf_file):
+
+def install_api_server(conf_file):
     config = ConfigParser.ConfigParser()
     config.read(conf_file)
+    api_path = config.get('Global', 'api_path')
     print(yellow("Warning : You need to execute this script with sudoer"))
     print(green("Installing linux packages..."))
-    sudo("apt-get install -y nginx supervisort python-dev python-virtualenv")
+    sudo("apt-get install -y nginx supervisor python-dev python-virtualenv git")
     print(green("Linux packages installed"))
     print(green("Creating project path"))
-    api_path = config.get('Global', 'api_path')
-    sudo("mkdir {0}".format(api_path))
+    run("git clone https://karec@bitbucket.org/karec/ics2web.git {0}".format(api_path))
     with cd(api_path):
         run("virtualenv ics2web")
     print(green("setting up virtualenv"))
-    source = "source {0} && ".format(api_path + "ics2web")
+    source = "source {0} && ".format(api_path + "/ics2web/bin/activate")
     print(green("Installing requirements for project"))
     run(source + "pip install -r " + api_path + "/scripts/requirements.txt")
     run(source + "pip install gunicorn")
@@ -25,16 +28,18 @@ def install_api_serveur(conf_file):
         content = f.read()
         content = content.format(api_path, config.get('Global', 'user'),
                                  config.get('Global', 'group'),
-                                 config.get('Global', 'workders'))
-        run('echo "{0}" > {1}'.format(content, api_path + '/ics2web/run/gunicorn_start'))
-        run('chmod a+x ' + api_path + '/ics2web/run/gunicorn_start')
+                                 config.getint('Global', 'workers'))
+        run('touch {0}'.format(api_path + '/ics2web/bin/gunicorn_start'))
+        append(api_path + '/ics2web/bin/gunicorn_start', content, escape=True)
+        run('chmod a+x ' + api_path + '/ics2web/bin/gunicorn_start')
     print(green("Configuring nginx"))
     with open(config.get('Files', 'nginx_template')) as f:
         content = f.read()
         content = content.format(api_path, config.get('Nginx', 'access_log'),
                                  config.get('Nginx', 'error_log'),
                                  config.get('Nginx', 'server_name'))
-        sudo('echo "{0}" > {1}'.format(content, '/etc/nginx/sites-available/ics2web'))
+        sudo("touch {0}".format("/etc/nginx/sites-available/ics2web"))
+        append('/etc/nginx/sites-available/ics2web', content, use_sudo=True)
         sudo("ln -s /etc/nginx/sites-available/ics2web /etc/nginx/sites-enabled/ics2web")
         sudo('service nginx restart')
     print(green("Configuring supervisor"))
@@ -44,6 +49,8 @@ def install_api_serveur(conf_file):
                                  config.get('Global', 'user'),
                                  config.get('Global', 'group'),
                                  config.get('Supervisor', 'log_file'))
-        sudo('echo "{0}" > {1}'.format(content, '/etc/supervisor/conf.d/ics2web.conf'))
+        sudo("touch {0}".format("/etc/supervisor/conf.d/ics2web.conf"))
+        append('/etc/supervisor/conf.d/ics2web.conf', content, use_sudo=True)
+        sudo('supervisorctl reload')
     print(green("Starting application"))
     sudo('supervisorctl start ics2web')
