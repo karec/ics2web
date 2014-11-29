aabebc0c4645fc0081322d6873886d37d617b2ed
Aurelien Pigot <aurel.pigot@gmail.com>
2014-11-29 17:34:19 +0100

    add slepp function (to fix)

--- client/js/my.js
+++ client/js/my.js
@@ -1,7 +1,8 @@
 var app = angular.module('icstoweb', []);
 
 
-var ics = "http://ics.evalette.net/api/get?url=www.google.com/calendar/ical/valett_e%40etna-alternance.net/private-dcbad4791bccb7846db0fdd38f9498f8/basic.ics"
+var ics = "http://ics.evalette.net/api/get/?url=https://www.google.com/calendar/ical/bene_t%40etna-alternance.net/private-ebd8846b2fae995953df0e5494323e82/basic.ics"
+
 
 
 
@@ -44,25 +45,48 @@ app.controller('icsCurrentCtrl', function($scope, $http, $timeout) {
 	 app.controller('icsNextCtrl', function($scope, $http, $timeout) {
 		$http.get(ics)
 		.success(function(response) {
-			var len = response.next_events.length -1;
+			var len = response.next_events.length;
 			var tmp = len;
 			var tab = [];
 
 			var myNext = function() {
 
+
+				function sleep(milliseconds) {
+					console.log("Im on sleep");
+				  var start = new Date().getTime();
+				  for (var i = 0; i < 1e7; i++) {
+				    if ((new Date().getTime() - start) > milliseconds){
+				      break;
+				    }
+				  }
+				}
+
 				while (len > 0){
-					for (var i = 0; i < 4; i++) {
+
+					var iter = 4
+
+					var offset = 0;
+					for (var i = offset; i < iter; i++) {
 					tab[i] = response.next_events[i];
 	
 					
 					};
-
+					$scope.next_eve = tab;
+					console.log($scope.next_eve);
+					offset = offset + iter;
+					console.log("offset "+offset);
+					len = len - 4;
+					console.log("new len "+len);
+					if (len < 4) {iter = len};
+					console.log("new iter "+iter);
+					sleep(2000);
 				}
 				
-				//$timeout(myNext, 1000);
+				//$timeout(myNext, 5000);
 			}
 			$timeout(myNext, 100);
-			$scope.next_eve = tab;
+			
 			var lentab = response.next_events.length -1;
 			$scope.lentab = lentab;
 		});
